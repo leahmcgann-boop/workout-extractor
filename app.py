fe4c24a9a425bda0c709b04e0b4618adcc893bfa
Leah <leahmcgann@gmail.com>
2026-04-03 10:06:48 -0400

    Fix host binding for Railway

--- app.py
+++ app.py
@@ -95,4 +95,5 @@ def index():
     return render_template('index.html', results=results)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
